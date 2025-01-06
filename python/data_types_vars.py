0108276e4f9fd673bd08c081c139f98204436309
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-01-06 14:53:53 +0000

    new commit

--- python/data_types_vars.py
+++ python/data_types_vars.py
@@ -34,3 +34,82 @@
 
 #print(t1() is t2())
 
+# scope
+
+#global_variable = "accessible everywhere"
+#x = 20
+
+#def my_function():
+    #global x 
+    #x = 10 # shadowing 
+  #  local_variable = "accessible only inside this function"
+ #   print(local_variable)
+ #   print(global_variable)
+
+#my_function()
+
+#print(x)
+
+# data types
+
+#int = 20
+#string = ""
+#float = 12.23
+#bool = True/False, 1/0, something/nothing
+
+# in built functions
+# print()
+# input()
+
+#import sys
+
+#with open("output.txt", "w") as file:
+    #sys.stdout = file
+    #print("go to the file instead pls!!")
+
+#sys.stdout = sys.__stdout__
+
+#print("hello")
+
+# escape chars
+
+# \ take the next symbol as its literal meaning
+# \t tab, \n newline, \u unicode, \U extended unicde, \\ backslash
+
+# print("Person1: \tHey how are you?\nPerson2\\: \tim good thanks \U0001f604")
+
+# string concatenation 
+
+#name = input("enter your name: ") # defaults to a string
+#age = int(input("enter your age: "))
+
+#message = "your name is {}, your age is {}".format(name, age)
+#print(message)
+
+#print("your name is " + name) # limited to same data type
+#print("your name is", name, "your age is", age)
+
+#print(f"your name is {name}, your age is {age}")
+
+# BODMAS 
+
+#floor division // - 10//3 = 3
+#% 10%3 = 1 # give the remainder. 
+
+# string methods
+
+#print("hello world".upper())
+#print("HELLO WORLD".lower())
+
+#print("hello world".count("o", 3, -3))
+
+#print("hello world world world".replace("world", "class", 2))
+
+#print("h#el#lo#wor#l#d".split("#"))
+
+
+
+
+
+
+
