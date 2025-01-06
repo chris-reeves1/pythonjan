a80ed28f564dc1d53517d7ef8c5612cfdf8197fd
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-01-06 12:49:55 +0000

    2

---
+++ data_types_vars.py
@@ -0,0 +1,36 @@
+# vars - a reference (a name), refere3nces a selection of memory (mem location)
+# protected location. 
+
+#x = 20
+#person_one_age = 20
+#personAge = 20 
+
+# consistancy is key!
+# pep-8 style guide. 
+
+# naming convention: should be decriptive, dont start with a number, not capitalised, no keywords!
+
+# 1age = not allowed!
+# Age = 10 reserved for class names
+# AGE = constants - just a convention. 
+# class_ = 10 avoid keywords
+
+#PascalCase - class names/file names
+#camelCase vars/functions/methods
+#snake_case vars/functions/methods
+
+#a = 1000
+#b = 1000
+
+
+#print(a is b) # interning 
+
+#def t1():
+#    c = int(str(260))
+#    return c
+#def t2():
+#    d = int(str(260))
+#    return d
+
+#print(t1() is t2())
+
