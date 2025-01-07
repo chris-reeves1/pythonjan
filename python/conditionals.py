59fe0219e2d88d406bc14a83e4ca30c5533b5e34
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-01-07 10:53:57 +0000

    new commit

--- python/conditionals.py
+++ python/conditionals.py
@@ -31,4 +31,56 @@
 # mark over 60 print pass
 # else fail.
 
+#grade = int(input('Enter Grade: '))
+
+#if grade > 80:
+#    print("Distinction")
+#elif grade > 60:
+#    print("Pass")
+#else:
+#    print("Fail")
+
+# multiple comparators
+
+#deposit = 999
+#password = "password1"
+
+#if 0 < deposit < 100 and password == "password":
+ #   print(f"thankyou for deposit of {deposit}")
+#else:
+#    print("failed to deposit")
+
+# not
+
+#if not 0 < deposit < 100 and password != "password":
+#    print("failed to depsit")
+#else:
+#    print("thanks")
+
+# in and not in
+
+#name = "root123"
+
+#if name in ("root", "admin","user"):
+ #   print("invalid username")
+#else:
+#    print("accepted")
+
+#if name not in ("root", "admin","user"):
+#    print("accepted")
+#else:
+#    print("invalid username")
+
+# exercise:
+#weight converter app
+#user to input weight
+#input for kgs or lbs
+#if statements to check the unit
+#convert the value
+#print out result
+
+#optional - error handling for upper/lower case + incorrrect unit input.
+
+#optional x2- error handling for numeric input (weight). 
+
 
