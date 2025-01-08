1c8fb3e301e74dda0db5d5bf39286777d212c3b4
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-01-08 11:42:04 +0000

    new commit

---
+++ python/feedback-task.py
@@ -0,0 +1,33 @@
+General comments
+Charles has good knowledge of programming principles and did well in this module. 
+He contributed to group discussion/questions and worked well in groups.  
+
+Learner Punctuality and engagement 
+Charles was always punctual throughout the module and engaged well with camera on. 
+
+Recommendations on further learning
+Continue to practice and explore good code design and start to specialise in 
+areas of interest. 
+
+user to input a list of names. 
+
+prompt the user for scores in the following areas (at least 2):
+- understanding level (1-5)
+- contribution level
+- lab completion level
+- engagament level
+- Punctuality level
+- furthe learning level
+
+dictionary to map scores to descriptive words. 
+
+template (string) "Gneral commnets/n{name} did {map[understanding_level]} in this module/n"
+template += "engagement and punctuality\n {name}....."
+template += ""
+
+output: 
+individual files for each students (student name is file name)
+optional - in its own directory
+
+optional stretch goal:
+- open the file for editing in a text editor for last minute changes/approval. 
