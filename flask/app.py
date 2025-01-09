a45e30f4ba1a1cab2214a833b46e7b45695afdbd
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-01-09 12:04:15 +0000

    new commit

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to the password rating app"
+    return render_template("home.html")
+
+#/check route
+@app.route("/check", methods=["POST"])
+def check():
+    password = request.form["password"]
+    return render_template("result.html", password=password, rating="to be implemeted")
 
 if __name__=="__main__":
     app.run(debug=True, port=5000)
