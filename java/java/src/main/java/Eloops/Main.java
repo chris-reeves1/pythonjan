9df91d612bc6597ec18f3bedfc5b6b67db33e76a
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-01-13 14:24:54 +0000

    new commit

--- java/java/src/main/java/Eloops/Main.java
+++ java/java/src/main/java/Eloops/Main.java
@@ -1,16 +1,9 @@
 package Eloops;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         
-        int i = 1;
-while (i <= 5) {
-    System.out.println("Count: " + i);
-    i++;
-}
-
-
-
-
-    }
+    } 
 }
---
+++ java/java/src/main/java/Farrays/Main.java
@@ -0,0 +1,5 @@
+package Farrays;
+
+public class Main {
+
+}
