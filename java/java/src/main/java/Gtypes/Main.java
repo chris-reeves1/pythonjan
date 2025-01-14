cdca129934b8d94db1e56927333cba314c538831
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-01-14 12:58:38 +0000

    new commit

--- java/java/src/main/java/Gtypes/Main.java
+++ java/java/src/main/java/Gtypes/Main.java
@@ -2,8 +2,22 @@ package Gtypes;
 
 public class Main {
     public static void main(String[] args) {
+
+    //person1.introduce();
+    Person child = Person.createPerson("chris");
+    System.out.println(child);
+
+    Pizza veggie = Pizza.createVeggie();
+    Pizza m = Pizza.createMargherita();
+    System.out.println(m);
+    System.out.println(veggie);
+    
+    
     }
 }
+    
+    
+        
     
 
 
