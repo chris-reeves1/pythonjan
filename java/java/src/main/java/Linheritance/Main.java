27c5c3e62d58abbe91709aee87f72da2e062a9b3
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-01-16 11:14:55 +0000

    new commit

--- java/java/src/main/java/Linheritance/Main.java
+++ java/java/src/main/java/Linheritance/Main.java
@@ -2,7 +2,25 @@ package Linheritance;
 
 public class Main {
     public static void main(String[] args) {
-    
+        Animal myDog = new Dog(); // Substitution (upcasting)
+        if (myDog instanceof Dog) {
+            Dog myDog1 = (Dog) myDog; // Safe casting
+            myDog1.test(); 
+            myDog.speak();}
+    }
+}
+class Animal {
+    public void speak() {
+        System.out.println("Animal speaks.");
     }
 }
 
+class Dog extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("Dog barks.");
+    }
+    public void test(){
+        System.out.println("output");
+    }
+}
